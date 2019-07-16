4d8bc95c2e6bfe53c91cec7c88d592018f1df66b
lamhoangtung <lamhoangtung.vz@gmail.com>
2019-07-16 11:39:49 +0700

    Minor changes

--- cinnamon_setup.sh
+++ cinnamon_setup.sh
@@ -2,10 +2,11 @@ git clone https://github.com/lamhoangtung/ultra_high_resolution_segmentation
 cd ultra_high_resolution_segmentation
 mkdir data/
 cd data/
-drive clone 10l8C62xlUM8mMDUheadbwPCZTUY-S9yA
+drive clone 1Fxn1pjoJNRVNvEiDRTcLu8ZDItI3JUVS
 unzip all_prj.zip
 cd ..
 mkdir experiments
 cd experiments
 mkdir cinnamon
 cd ../..
+mkdir saved_models
