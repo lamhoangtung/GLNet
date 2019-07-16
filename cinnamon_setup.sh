e181574de37301f5c74dae80de8be9abb29b0797
lamhoangtung <lamhoangtung.vz@gmail.com>
2019-07-16 09:58:34 +0700

    Cinnamon quick setup script

---
+++ cinnamon_setup.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/lamhoangtung/ultra_high_resolution_segmentation
+cd ultra_high_resolution_segmentation
+mkdir data/
+cd data/
+drive clone 10l8C62xlUM8mMDUheadbwPCZTUY-S9yA
+unzip all_prj.zip
+cd ..
+mkdir experiments
+cd experiments
+mkdir cinnamon
+cd ../..
