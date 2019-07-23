f4f12702a63d1f8e7d1fb0bbfadf638d1de99baf
lamhoangtung <linus@cinnamon.is>
2019-07-23 03:44:00 +0000

    Mode 2 only work with batch size 1

--- train_cinnamon.sh
+++ train_cinnamon.sh
@@ -6,14 +6,14 @@ python3 train_cinnamon.py \
 --log_path "./experiments/cinnamon/" \
 --task_name "fpn_global.508_4.28.2019_lr2e5" \
 --mode 2 \
---batch_size 6 \
+--batch_size 1 \
 --sub_batch_size 6 \
 --size_g 508 \
 --size_p 508 \
 --path_g "fpn_global.508_4.28.2019_lr2e5.pth" \
 --path_g2l "fpn_global2local.508.deep.cat.1x_ensemble_fmreg.p3_10.14.2018.lr2e5.pth" \
 --path_l2g "fpn_local2global.508_deep.cat_ensemble.p3_10.31.2018.lr2e5.local1x.epoch13.pth" \
---num_workers 16
+--num_workers 0
 # --path_g "cityscapes_global.800_4.5.2019.lr5e5.pth" \
 # --path_g2l "fpn_global2local.508_deep.cat.1x_fmreg_ensemble.p3.0.15l2_3.19.2019.lr2e5.pth" \
 # --path_l2g "fpn_local2global.508_deep.cat.1x_fmreg_ensemble.p3_3.19.2019.lr2e5.pth" \
